f1bba417e0209c9681a93bcda1b92ddb75b61153
Gurkan TURGAY <gurkan.turgay29@gmail.com>
2024-08-22 13:49:27 +0300

    layout ve sayfa tasarımı

---
+++ DepartmanProjesi/Controllers/departController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DepartmanProjesi.Controllers
+{
+    public class departController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
